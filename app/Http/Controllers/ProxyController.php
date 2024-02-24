8725f97209c555b93e96b029a9aa84cd02d3bf55
Jan Willem Kaper <kapersoft@gmail.com>
2024-02-24 15:49:03 +0100

    Add proxy for https://pinkary.com/@kapersoft

---
+++ app/Http/Controllers/ProxyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Response;
+
+class ProxyController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        if ($request->path() === '/') {
+            $url = "https://pinkary.com/@kapersoft";
+        } else {
+            $url = "https://pinkary.com/{$request->path()}";
+        }
+
+        ['body' => $body, 'headers' => $headers] = Cache::remember(
+            $request->path(),
+            now()->addHour(),
+            function () use ($url) {
+            $response = Http::get($url);
+                return [
+                    'body'  => $response->body(),
+                    'headers' => $response->headers(),
+                ];
+            }
+        );
+
+        $body = (string)str($body)->replace('https://pinkary.com', $request->schemeAndHttpHost());
+
+        return Response::make($body, 200, [
+            'Content-Type' => $headers['Content-Type'],
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProxyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::fallback(ProxyController::class);
